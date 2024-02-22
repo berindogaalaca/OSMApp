2d20e24d3f90864e4a146e74862f7b02d20f9801
Berin <dogaalacaaa@gmail.com>
2024-02-22 20:55:37 +0300

    update businessLayer

---
+++ BusinessLayer/Concrete/PointManager.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class PointManager : IPointService
+    {
+        IPointDal _PointDal;
+        public PointManager(IPointDal pointDal)
+        {
+            _PointDal = pointDal;
+        }
+        public List<Point> GetList()
+        {
+           return _PointDal.GetList();
+        }
+
+        public void TAdd(Point t)
+        {
+           _PointDal.Insert(t);
+        }
+
+        public void TDelete(Point t)
+        {
+            _PointDal.Delete(t);
+        }
+
+        public Point TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Point t)
+        {
+            _PointDal.Update(t);
+        }
+    }
+}
