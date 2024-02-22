ffe2448c048af5a86c8494bc146108741cc7a7a9
Berin <dogaalacaaa@gmail.com>
2024-02-22 20:44:30 +0300

    add EntityFramework

--- DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
+++ DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e344266ef82dd0df50ab6eea33a31fdb9683796a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ed2ef14b60011b0a36bb551cc5a8e86dc9046e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
